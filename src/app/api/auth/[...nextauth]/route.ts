03925a699adbf04712b995c413820a67f7ef2f49
svyatoslavw <sadness0v2@gmail.com>
2024-09-30 13:32:16 +0300

    fix: fixed prisma schema

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,5 +2,5 @@ import { handlers } from "@/auth"
 
 export const { GET, POST } = handlers
 
-export const runtime = "nodejs"
+export const runtime = "edge"
 // export const runtime = process.env.NODE_ENV === "production" ? "edge" : "nodejs"
