74d2c3ef9d3edc10b8d2bbbb13b4aa9b1eb12071
svyatoslavw <sadness0v2@gmail.com>
2024-10-12 23:06:50 +0300

    chore: fixed middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -21,19 +21,5 @@ export default auth((req: NextRequest & { auth: TSession }) => {
 })
 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - favicon.ico (favicon file)
-     * - icon.png (icon file)
-     * - apple-icon.png (Apple icon file)
-     * - workbox* (service worker files)
-     * - sw* (service worker files)
-     * - sitemap.xml (sitemap file)
-     * - robots.txt (robots file)
-     * - manifest.webmanifest (web manifest file)
-     */
-    "/((?!api|favicon.ico|icon.png|apple-icon.png|workbox.*|sw.*|sitemap.xml|robots.txt|manifest.webmanifest).*)"
-  ]
+  matcher: ["/", "/settings", "/resume/:path*"]
 }
