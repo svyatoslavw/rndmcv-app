2a5a5cc77ed4b5ceeec5f80497f803e9a47963f9
Sviatoslav Stepanov <sadness0v2@gmail.com>
2025-01-28 14:08:42 +0200

    chore: fixed pre-deploy workflow

--- packages/database/generated/client/index.js
+++ packages/database/generated/client/index.js
@@ -225,7 +225,7 @@ const config = {
     isCustomOutput: true
   },
   relativeEnvPaths: {
-    rootEnvPath: "../../.env",
+    rootEnvPath: null,
     schemaEnvPath: "../../.env"
   },
   relativePath: "../../prisma",
