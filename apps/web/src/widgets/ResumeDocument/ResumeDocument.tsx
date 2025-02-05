4863deb96ea89092dacc77ae9df3ba20ada1ccab
Sviatoslav Stepanov <sadness0v2@gmail.com>
2025-02-05 20:32:56 +0200

    fix: fixed doc

--- apps/web/src/widgets/ResumeDocument/ResumeDocument.tsx
+++ apps/web/src/widgets/ResumeDocument/ResumeDocument.tsx
@@ -1,5 +1,8 @@
 import { AspectRatio } from "@rndm/ui/components"
 
+import { ResumeDomain } from "@/entities/resume"
+import { uuid } from "@/shared/lib/utils"
+import { ICustomization, IGeneral } from "@/shared/types"
 import { DocumentPage } from "./DocumentPage"
 import { DocumentSide } from "./DocumentSide"
 import { EducationBlock } from "./EducationBlock"
@@ -8,9 +11,6 @@ import { LanguagesBlock } from "./LanguagesBlock"
 import { PersonBlock } from "./PersonBlock"
 import { ProjectsBlock } from "./ProjectsBlock"
 import { SkillsBlock } from "./SkillsBlock"
-import { ResumeDomain } from "@/entities/resume"
-import { uuid } from "@/shared/lib/utils"
-import { ICustomization, IGeneral } from "@/shared/types"
 
 interface ResumeDocumentProps {
   className?: string
@@ -48,133 +48,135 @@ const ResumeDocument = ({
           lineHeight={spacing.lineHeight}
           mode={colors.mode}
         >
-          {layout.columns.left &&
-            layout.columns.left.map((block) => (
-              <DocumentSide
-                key={block + "-left"}
-                colors={colors}
-                isCard={isCard}
-                layout={layout}
-                spacing={spacing}
-                variant="left"
-              >
-                {block === "person" && (
-                  <PersonBlock
-                    isLeft
-                    customization={customization}
-                    isCard={isCard}
-                    person={general.person}
-                  />
-                )}
+          <DocumentSide
+            colors={colors}
+            isCard={isCard}
+            layout={layout}
+            spacing={spacing}
+            variant="left"
+          >
+            {layout.columns.left &&
+              layout.columns.left.map((block) => (
+                <div key={block + "-left"}>
+                  {block === "person" && (
+                    <PersonBlock
+                      isLeft
+                      customization={customization}
+                      isCard={isCard}
+                      person={general.person}
+                    />
+                  )}
 
-                {isInclude(block, "education") && (
-                  <EducationBlock
-                    isLeft
-                    customization={customization}
-                    education={general.education}
-                    isCard={isCard}
-                  />
-                )}
+                  {isInclude(block, "education") && (
+                    <EducationBlock
+                      isLeft
+                      customization={customization}
+                      education={general.education}
+                      isCard={isCard}
+                    />
+                  )}
 
-                {isInclude(block, "experience") && (
-                  <ExperienceBlock
-                    isLeft
-                    customization={customization}
-                    experience={general.experience}
-                    isCard={isCard}
-                  />
-                )}
+                  {isInclude(block, "experience") && (
+                    <ExperienceBlock
+                      isLeft
+                      customization={customization}
+                      experience={general.experience}
+                      isCard={isCard}
+                    />
+                  )}
 
-                {isInclude(block, "projects") && (
-                  <ProjectsBlock
-                    customization={customization}
-                    isCard={isCard}
-                    projects={general.projects}
-                  />
-                )}
+                  {isInclude(block, "projects") && (
+                    <ProjectsBlock
+                      customization={customization}
+                      isCard={isCard}
+                      projects={general.projects}
+                    />
+                  )}
 
-                {isInclude(block, "skills") && (
-                  <SkillsBlock
-                    isLeft
-                    customization={customization}
-                    isCard={isCard}
-                    skills={general.skills}
-                  />
-                )}
+                  {isInclude(block, "skills") && (
+                    <SkillsBlock
+                      isLeft
+                      customization={customization}
+                      isCard={isCard}
+                      skills={general.skills}
+                    />
+                  )}
 
-                {isInclude(block, "languages") && (
-                  <LanguagesBlock
-                    isLeft
-                    customization={customization}
-                    isCard={isCard}
-                    languages={general.languages}
-                  />
-                )}
-              </DocumentSide>
-            ))}
+                  {isInclude(block, "languages") && (
+                    <LanguagesBlock
+                      isLeft
+                      customization={customization}
+                      isCard={isCard}
+                      languages={general.languages}
+                    />
+                  )}
+                </div>
+              ))}
+          </DocumentSide>
 
-          {layout.columns.right &&
-            layout.columns.right.map((block) => (
-              <DocumentSide
-                key={block + "-right"}
-                colors={colors}
-                isCard={isCard}
-                layout={layout}
-                spacing={spacing}
-                variant="right"
-              >
-                {/* {block === "person" && (
-                  <PersonBlock
-                    customization={customization}
-                    isCard={isCard}
-                    person={general.person}
-                  />
-                )} */}
+          <DocumentSide
+            colors={colors}
+            isCard={isCard}
+            layout={layout}
+            spacing={spacing}
+            variant="right"
+          >
+            {layout.columns.right &&
+              layout.columns.right.map((block) => (
+                <div key={block + "-right"}>
+                  {block === "person" && (
+                    <PersonBlock
+                      customization={customization}
+                      isCard={isCard}
+                      person={general.person}
+                    />
+                  )}
 
-                {block === "education" && vb.includes("education") && (
-                  <EducationBlock
-                    customization={customization}
-                    education={general.education}
-                    isCard={isCard}
-                  />
-                )}
+                  {block === "education" && vb.includes("education") && (
+                    <EducationBlock
+                      customization={customization}
+                      education={general.education}
+                      isCard={isCard}
+                    />
+                  )}
 
-                {block === "experience" && vb.includes("experience") && (
-                  <ExperienceBlock
-                    isLeft
-                    customization={customization}
-                    experience={general.experience}
-                    isCard={isCard}
-                  />
-                )}
+                  {block === "experience" && vb.includes("experience") && (
+                    <ExperienceBlock
+                      isLeft
+                      customization={customization}
+                      experience={general.experience}
+                      isCard={isCard}
+                    />
+                  )}
 
-                {block === "projects" && vb.includes("projects") && (
-                  <ProjectsBlock
-                    customization={customization}
-                    isCard={isCard}
-                    projects={general.projects}
-                  />
-                )}
+                  {block === "projects" && vb.includes("projects") && (
+                    <ProjectsBlock
+                      customization={customization}
+                      isCard={isCard}
+                      projects={general.projects}
+                    />
+                  )}
 
-                {block === "skills" && vb.includes("skills") && (
-                  <SkillsBlock
-                    isLeft
-                    customization={customization}
-                    isCard={isCard}
-                    skills={general.skills}
-                  />
-                )}
+                  {block === "skills" && vb.includes("skills") && (
+                    <SkillsBlock
+                      isLeft
+                      customization={customization}
+                      isCard={isCard}
+                      skills={general.skills}
+                    />
+                  )}
 
-                {block === "languages" && vb.includes("languages") && (
-                  <LanguagesBlock
-                    isLeft
-                    customization={customization}
-                    isCard={isCard}
-                    languages={general.languages}
-                  />
-                )}
-              </DocumentSide>
-            ))}
+                  {block === "languages" && vb.includes("languages") && (
+                    <LanguagesBlock
+                      isLeft
+                      customization={customization}
+                      isCard={isCard}
+                      languages={general.languages}
+                    />
+                  )}
+                </div>
+              ))}
+          </DocumentSide>
         </DocumentPage>
       </div>
     </AspectRatio>
