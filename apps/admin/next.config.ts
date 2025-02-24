6af2af947ddfd28eda98313442499c357672bfd0
Sviatoslav Stepanov <127883727+svyatoslavw@users.noreply.github.com>
2025-02-24 19:49:14 +0200

    fix: updated next.config

--- apps/admin/next.config.ts
+++ apps/admin/next.config.ts
@@ -7,7 +7,8 @@ const nextConfig: NextConfig = {
     AUTH_GITHUB_ID: process.env.AUTH_GITHUB_ID,
     AUTH_GITHUB_SECRET: process.env.AUTH_GITHUB_SECRET,
     AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID,
-    AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET
+    AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET,
+    POSTGRES_URL: process.env.POSTGRES_URL
   },
   images: {
     remotePatterns: [
